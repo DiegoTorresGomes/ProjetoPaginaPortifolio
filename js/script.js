a551d6fb25b8b4587889c149670cd73897cf455a
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-10-16 20:12:05 -0300

    Portifolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* tipos de animações */
+
+var typed = new Typed(".typing",{
+    strings:[" ","Web Designer", "Graphic Designer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
