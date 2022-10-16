400803ef9d5cf92357650040ca490d1f0619ec45
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-10-16 19:15:04 -0300

    Portifolio

---
+++ js/style-switcher.js
@@ -0,0 +1,24 @@
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+window.addEventListener("scroll", () =>{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyle.forEach((style) =>{
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
